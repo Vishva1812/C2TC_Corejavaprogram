84e9e0019772d36111202193dc111b242e84fc94
Dell <Dell@DESKTOP-UDAHKSB>
2025-07-21 22:51:33 +0530

    Day One Programs

---
+++ C2TC__codejavaprogram/src/DayOne/CharDemo.java
@@ -0,0 +1,23 @@
+package DayOne;
+
+public class CharDemo {
+
+	public static void main(String[] args) {
+		char ch = 's';
+		System.out.println(ch);
+		
+		char ch2=65;
+		System.out.println(ch2);
+		
+		char var1 ='\u00A7';
+		System.out.println(var1);
+		
+		char var2 ='\u20AC';
+		System.out.println(var2);
+		
+		int a ='A';
+		System.out.println(a);
+		
+	}
+
+}
---
+++ C2TC__codejavaprogram/src/DayOne/PrimitiveDataTypes.java
@@ -0,0 +1,10 @@
+package DayOne;
+
+public class PrimitiveDataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

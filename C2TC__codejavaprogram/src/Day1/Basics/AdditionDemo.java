add2676f91c87c1ca02766d88d6f653c84ec305b
Dell <Dell@DESKTOP-UDAHKSB>
2025-07-21 22:25:31 +0530

    ClassObject

---
+++ C2TC__codejavaprogram/src/Day1/Basics/AdditionDemo.java
@@ -0,0 +1,12 @@
+package Day1.Basics;
+import java.util.Scanner;
+public class AdditionDemo {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		System.out.println(a);
+
+	}
+
+}

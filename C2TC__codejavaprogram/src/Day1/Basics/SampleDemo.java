57acd055433f22d4bc020c1816488d03fcb0b8d3
Dell <Dell@DESKTOP-UDAHKSB>
2025-07-21 23:11:45 +0530

    Day One Basic

--- C2TC__codejavaprogram/src/Day1/Basics/SampleDemo.java
+++ C2TC__codejavaprogram/src/Day1/Basics/SampleDemo.java
@@ -1,6 +1,6 @@
 package Day1.Basics;
 
-public class AdditionDemo {
+public class SampleDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- C2TC__codejavaprogram/src/Day1/Basics/SampleDemo2.java
+++ C2TC__codejavaprogram/src/Day1/Basics/SampleDemo2.java
@@ -1,6 +1,6 @@
 package Day1.Basics;
 
-public class AdditionDemo {
+public class SampleDemo2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

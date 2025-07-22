129a76c12ebc4060ea3f13ab5a1b6b0d06963025
Vishva <vishvavelleappan@gmail.com>
2025-07-22 12:26:04 +0530

    Entity

--- C2TC__codejavaprogram/src/Day1/Entity/CustomerDemo.java
+++ C2TC__codejavaprogram/src/Day1/Entity/CustomerDemo.java
@@ -3,7 +3,11 @@ package Day1.Entity;
 public class CustomerDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Customer c1= new Customer();
+		c1.setCid(101);
+		c1.setName("jack");
+		c1.setCity("pondy");
+		System.out.println(c1);
 
 	}
 
